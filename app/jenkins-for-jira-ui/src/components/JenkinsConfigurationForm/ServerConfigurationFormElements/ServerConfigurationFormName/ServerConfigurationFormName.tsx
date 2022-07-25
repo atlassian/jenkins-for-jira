d33f3fa898cb39902cce8237cbe47d85c715f6c9
yvonnevictoria <28718897+yvonnevictoria@users.noreply.github.com>
2022-07-25 16:35:07 +1000

    ARC-1264 Move Jenkins Server text field header to ServerConfigurationFormName component

--- app/jenkins-for-jira-ui/src/components/JenkinsConfigurationForm/ServerConfigurationFormElements/ServerConfigurationFormName/ServerConfigurationFormName.tsx
+++ app/jenkins-for-jira-ui/src/components/JenkinsConfigurationForm/ServerConfigurationFormElements/ServerConfigurationFormName/ServerConfigurationFormName.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Textfield from '@atlaskit/textfield';
 import {
 	ErrorMessage,
 	Field
 } from '@atlaskit/form';
 import {
+	StyledInputHeaderContainer,
 	StyledTextfieldErrorContainer
 } from '../../JenkinsConfigurationForm.styles';
+import { FormTooltip } from '../../../Tooltip/Tooltip';
 
 type ServerConfigurationFormNameProps = {
 	serverName: string;
@@ -24,27 +26,37 @@ const ServerConfigurationFormName = ({
 	setHasError
 }: ServerConfigurationFormNameProps) => {
 	return (
-		<StyledTextfieldErrorContainer hasError={hasError}>
-			<Field label='Server name' name='server-name-label'>
-				{() => (
-					<Textfield
-						name='server-name'
-						value={serverName}
-						aria-label='server name field'
-						testId='server-name'
-						onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-							setServerName(event);
-							setHasError(false);
-						}}
-					/>
+		<Fragment>
+			<StyledInputHeaderContainer>
+				<h3>Jenkins server </h3>
+				<FormTooltip
+					content='Create a display name for your Jenkins server.'
+					label='Jenkins Server'
+				/>
+			</StyledInputHeaderContainer>
+
+			<StyledTextfieldErrorContainer hasError={hasError}>
+				<Field label='Server name' name='server-name-label'>
+					{() => (
+						<Textfield
+							name='server-name'
+							value={serverName}
+							aria-label='server name field'
+							testId='server-name'
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+								setServerName(event);
+								setHasError(false);
+							}}
+						/>
+					)}
+				</Field>
+				{ hasError && (
+					<ErrorMessage testId='error-message'>
+						{errorMessage}
+					</ErrorMessage>
 				)}
-			</Field>
-			{hasError && (
-				<ErrorMessage testId='error-message'>
-					{errorMessage}
-				</ErrorMessage>
-			)}
-		</StyledTextfieldErrorContainer>
+			</StyledTextfieldErrorContainer>
+		</Fragment>
 	);
 };
 
