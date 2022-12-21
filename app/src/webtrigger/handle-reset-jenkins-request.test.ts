eafe26a6b5c9b6bff8a65460ffb1c3d8644a9595
Harminder <hsingh5@atlassian.com>
2022-12-21 14:39:30 +1100

    Initial checkin

--- app/src/webtrigger/handle-reset-jenkins-request.test.ts
+++ app/src/webtrigger/handle-reset-jenkins-request.test.ts
@@ -2,7 +2,7 @@ import { when } from 'jest-when';
 import { getAllJenkinsServers } from '../storage/get-all-jenkins-servers';
 import { mockSingleJenkinsPipeline } from '../storage/mockData';
 import { disconnectJenkinsServer } from '../storage/disconnect-jenkins-server';
-import { resetJenkinsServer } from './handle-reset-jenkins-request';
+import { resetJenkinsServer, deleteBuildsAndDeployments } from './handle-reset-jenkins-request';
 import { deleteBuilds } from '../jira-client/delete-builds';
 import { deleteDeployments } from '../jira-client/delete-deployments';
 
@@ -14,8 +14,12 @@ jest.mock('../jira-client/delete-deployments');
 const CLOUD_ID = '97eaf652-4b6e-46cf-80c2-d99327a63bc1';
 
 describe('Reset Jenkins Server Suite', () => {
-	when(disconnectJenkinsServer).mockImplementation(() => Promise.resolve(true));
-	when(getAllJenkinsServers).mockImplementation(() => Promise.resolve([mockSingleJenkinsPipeline]));
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		when(disconnectJenkinsServer).mockImplementation(() => Promise.resolve(true));
+		when(getAllJenkinsServers).mockImplementation(() => Promise.resolve([mockSingleJenkinsPipeline]));
+	});
 
 	it('Should delete all Jenkins server configuration from Forge Storage', async () => {
 		await resetJenkinsServer(CLOUD_ID);
@@ -24,4 +28,19 @@ describe('Reset Jenkins Server Suite', () => {
 		expect(deleteBuilds).toBeCalledWith(CLOUD_ID, mockSingleJenkinsPipeline.uuid);
 		expect(deleteDeployments).toBeCalledWith(CLOUD_ID, mockSingleJenkinsPipeline.uuid);
 	});
+
+	it('Should not delete excluded server from Forge Storage', async () => {
+		await resetJenkinsServer(CLOUD_ID, mockSingleJenkinsPipeline.uuid);
+		expect(getAllJenkinsServers).toBeCalled();
+		expect(disconnectJenkinsServer).toBeCalledTimes(0);
+		expect(deleteBuilds).toBeCalledTimes(0);
+		expect(deleteDeployments).toBeCalledTimes(0);
+	});
+
+	it('Should delete builds and deployments', async () => {
+		await deleteBuildsAndDeployments(CLOUD_ID, mockSingleJenkinsPipeline.uuid);
+		expect(deleteBuilds).toBeCalledWith(CLOUD_ID, mockSingleJenkinsPipeline.uuid);
+		expect(deleteDeployments).toBeCalledWith(CLOUD_ID, mockSingleJenkinsPipeline.uuid);
+	});
+
 });
--- app/src/webtrigger/handle-reset-jenkins-request.ts
+++ app/src/webtrigger/handle-reset-jenkins-request.ts
@@ -26,8 +26,15 @@ async function handleResetJenkinsRequest(
 		const jenkinsRequest = payload as JenkinsRequest;
 		if (jenkinsRequest.requestType === RequestType.RESET_JENKINS_SERVER) {
 			const cloudId = extractCloudId(context.installContext);
-			await resetJenkinsServer(cloudId);
+			await resetJenkinsServer(cloudId, jenkinsRequest.data?.excludeUuid);
 			return createWebtriggerResponse(200, '{"success": true}');
+		} else if (jenkinsRequest.requestType === RequestType.DELETE_BUILDS_DEPLOYMENTS) {
+			const cloudId = extractCloudId(context.installContext);
+			if (jenkinsRequest.data?.uuid) {
+				await deleteBuildsAndDeployments(cloudId, jenkinsRequest.data.uuid);
+				return createWebtriggerResponse(200, '{"success": true}');
+			}
+			return createWebtriggerResponse(400, '{"error": "No uuid found"}');
 		}
 		throw new InvalidPayloadError(`unsupported request type ${jenkinsRequest.requestType}`);
 	} catch (error) {
@@ -35,10 +42,13 @@ async function handleResetJenkinsRequest(
 	}
 }
 
-async function resetJenkinsServer(cloudId: string) {
+async function resetJenkinsServer(cloudId: string, excludeUuid?: string) {
 	try {
-		const jenkinsServers = await getAllJenkinsServers();
+		let jenkinsServers = await getAllJenkinsServers();
 		const disconnectJenkinsServerPromises: Array<Promise<any>> = [];
+		if (excludeUuid) {
+			jenkinsServers = jenkinsServers.filter((jenkinsServer) => jenkinsServer.uuid !== excludeUuid);
+		}
 		jenkinsServers.forEach((jenkinsServer) => {
 			const jenkinsServerUuid = jenkinsServer.uuid;
 			const disconnectJenkinsServerPromise = Promise.all([
@@ -55,4 +65,14 @@ async function resetJenkinsServer(cloudId: string) {
 	}
 }
 
-export { handleResetJenkinsRequest, resetJenkinsServer };
+async function deleteBuildsAndDeployments(cloudId: string, uuid: string) {
+	try {
+		await deleteBuilds(cloudId, uuid);
+		await deleteDeployments(cloudId, uuid);
+	} catch (error) {
+		console.error('unexpected error during deleteBuildsAndDeployments invocation', error);
+		throw error;
+	}
+}
+
+export { handleResetJenkinsRequest, resetJenkinsServer, deleteBuildsAndDeployments };
