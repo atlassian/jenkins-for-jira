37f2d14f6ae479641b5e32b9f9b423d1c42eb3b4
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-07-07 16:07:45 +1000

    testing hooks

--- app/jenkins-for-jira-ui/src/api/disconnectJenkinsServer.ts
+++ app/jenkins-for-jira-ui/src/api/disconnectJenkinsServer.ts
@@ -1,5 +1,6 @@
 import { invoke } from '@forge/bridge';
 
 export const disconnectJenkinsServer = async (uuid: string): Promise<boolean> => {
+	console.log('testing a change');
 	return invoke('disconnectJenkinsServer', { uuid });
 };
