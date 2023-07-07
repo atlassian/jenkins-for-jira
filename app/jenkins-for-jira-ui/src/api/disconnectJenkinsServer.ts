59cb28d19f93f1e818605d863ddee7d3972c824c
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-07-07 16:18:10 +1000

    add pre commit hook

--- app/jenkins-for-jira-ui/src/api/disconnectJenkinsServer.ts
+++ app/jenkins-for-jira-ui/src/api/disconnectJenkinsServer.ts
@@ -1,6 +1,5 @@
 import { invoke } from '@forge/bridge';
 
 export const disconnectJenkinsServer = async (uuid: string): Promise<boolean> => {
-	console.log('testing a change');
 	return invoke('disconnectJenkinsServer', { uuid });
 };
