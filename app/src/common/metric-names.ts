a51679de24f272252f352ffb62a5e00648416505
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-08-07 12:21:53 +1000

    address PR comments

--- app/src/common/metric-names.ts
+++ app/src/common/metric-names.ts
@@ -2,9 +2,9 @@ const server = 'jenkins-for-jira.forge.server';
 
 export const metricResolverEmitter = {
 	fetchJenkinsEventHandlerUrl: `${server}.fetchServer.emitted`,
-	connectJenkinsServer: `${server}.createServer.emitted`,
+	connectJenkinsServer: `${server}.connectServer.emitted`,
 	updateJenkinsServer: `${server}.updateServer.emitted`,
 	getAllJenkinsServers: `${server}.getAllServers.emitted`,
-	getJenkinsServerWithSecret: `${server}.getServer.emitted`,
+	getJenkinsServerWithSecret: `${server}.getWithSecretServer.emitted`,
 	disconnectJenkinsServer: `${server}.disconnectServer.emitted`
 };
