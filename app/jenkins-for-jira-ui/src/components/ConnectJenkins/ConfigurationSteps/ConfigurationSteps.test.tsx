67914a75893f1559694b7f86f287cc25b4af3960
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-08-21 20:43:29 +1000

    fix test

--- app/jenkins-for-jira-ui/src/components/ConnectJenkins/ConfigurationSteps/ConfigurationSteps.test.tsx
+++ app/jenkins-for-jira-ui/src/components/ConnectJenkins/ConfigurationSteps/ConfigurationSteps.test.tsx
@@ -5,6 +5,7 @@ import { ConfigurationSteps } from './ConfigurationSteps';
 
 describe('ConfigurationSteps Page Suite', () => {
 	it('Should render three stages', () => {
+
 		render(<ConfigurationSteps currentStage="install" />);
 		expect(screen.getByText('Install plugin')).toBeInTheDocument();
 		expect(screen.getByText('Create server')).toBeInTheDocument();
