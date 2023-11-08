2906e2533e37916030eb2329298223c92334a6fc
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-11-08 13:48:09 +1100

    fix test and ignore stoopid eslint lint

--- app/src/utils/extract-app-id-from-local-id.test.ts
+++ app/src/utils/extract-app-id-from-local-id.test.ts
@@ -3,16 +3,14 @@ import { extractAppIdFromLocalId } from './extract-app-id-from-local-id';
 describe('extractAppIdFromLocalId', () => {
     it('should extract the appId value from localId', () => {
         const officialAppId = 'df76f661-4cbe-4768-a119-13992dc4ce2d';
-        const officalLocalId = `
-            ari:cloud:ecosystem::extension/
-                ${officialAppId}/2113b3a2-5043-4d97-8db0-31d7e2379e3c/static/jenkins-for-jira-ui-admin-page`;
+        // eslint-disable-next-line max-len
+        const officalLocalId = `ari:cloud:ecosystem::extension/${officialAppId}/2113b3a2-5043-4d97-8db0-31d7e2379e3c/static/jenkins-for-jira-ui-admin-page`;
         const extractedOfficalAppId = extractAppIdFromLocalId(officalLocalId);
         expect(extractedOfficalAppId).toBe(officialAppId);
 
         const outdatedAppId = '21696c93-1a1d-4dd0-bac0-dfed80a62aba';
-        const outdatedLocalId = `
-            ari:cloud:ecosystem::extension/
-                ${outdatedAppId}/2113b3a2-5043-4d97-8db0-31d7e2379e3c/static/jenkins-for-jira-ui-admin-page`;
+        // eslint-disable-next-line max-len
+        const outdatedLocalId = `ari:cloud:ecosystem::extension/${outdatedAppId}/2113b3a2-5043-4d97-8db0-31d7e2379e3c/static/jenkins-for-jira-ui-admin-page`;
         const extractedOutdatedAppId = extractAppIdFromLocalId(outdatedLocalId);
         expect(extractedOutdatedAppId).toBe(outdatedAppId);
     });
--- app/src/utils/redirect-from-get-started.test.ts
+++ app/src/utils/redirect-from-get-started.test.ts
@@ -7,7 +7,8 @@ describe('redirectFromGetStarted', () => {
                 siteUrl: 'https://testjira.com',
                 appId: 'df76f661-4cbe-4768-a119-13992dc4ce2d',
                 environmentId: '1831-1281423-12389342983',
-                moduleKey: 'some-page'
+                moduleKey: 'some-page',
+                localId: 'ari:cloud:ecosystem::extension/df76f661-4cbe-4768-a119-13992dc4ce2d/2113b3a2-5043-4d97-8db0-31d7e2379e3c/static/jenkins-for-jira-ui-admin-page'
             },
         };
 
