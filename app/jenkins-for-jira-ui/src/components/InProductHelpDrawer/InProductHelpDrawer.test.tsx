f359bbcb56a8f14ca9f39ed39bd4f0f79572b03e
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-12-12 10:34:48 +1100

    merge base branch

--- app/jenkins-for-jira-ui/src/components/InProductHelpDrawer/InProductHelpDrawer.test.tsx
+++ app/jenkins-for-jira-ui/src/components/InProductHelpDrawer/InProductHelpDrawer.test.tsx
@@ -5,7 +5,12 @@ import { InProductHelpAction, InProductHelpActionType } from './InProductHelpAct
 describe('InProductHelpAction', () => {
 	test('renders InProductHelpAction with label', () => {
 		const { getByText } = render(
-			<InProductHelpAction label="build" type={InProductHelpActionType.HelpButton} appearance="primary" />
+			<InProductHelpAction
+				label="build"
+				type={InProductHelpActionType.HelpButton}
+				appearance="primary"
+				indexName="some name"
+			/>
 		);
 
 		expect(getByText('build')).toBeInTheDocument();
