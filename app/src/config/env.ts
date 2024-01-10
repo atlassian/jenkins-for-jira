2d566953e6bc4b11de8fb5e38969b601a90c6650
Josh Kay <jkay@atlassian.com>
2024-01-11 12:17:24 +1300

    clear key

--- app/src/config/env.ts
+++ app/src/config/env.ts
@@ -5,7 +5,7 @@ export interface EnvVars {
 }
 
 const envVars: EnvVars = {
-    LAUNCHDARKLY_API_KEY: 'api-1b02c331-80cf-483f-8809-2bd9eb79d192' || process.env.LAUNCHDARKLY_API_KEY || '',
+    LAUNCHDARKLY_API_KEY: process.env.LAUNCHDARKLY_API_KEY || '',
     LAUNCHDARKLY_APP_NAME: process.env.LAUNCHDARKLY_APP_NAME || 'jenkins-for-jira',
     JENKINS_ENV: process.env.JENKINS_ENV || ''
 };
