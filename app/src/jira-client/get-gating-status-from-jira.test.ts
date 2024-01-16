23fb0dada571f21af680ac5f0dc417604d955eef
Josh Kay <jkay@atlassian.com>
2024-01-17 12:24:57 +1300

    revert gating status api

--- app/src/jira-client/get-gating-status-from-jira.test.ts
+++ app/src/jira-client/get-gating-status-from-jira.test.ts
@@ -1,68 +1,96 @@
-import api from '@forge/api';
 import { Errors } from '../common/error-messages';
 import { getGatingStatusFromJira } from './get-gating-status-from-jira';
 import { InvalidPayloadError } from '../common/error';
 
-jest.mock('@forge/api', () => ({
-	...jest.requireActual('@forge/api'),
-	asApp: jest.fn().mockReturnValue({
-		requestConnectedData: jest.fn()
-	})
-}));
-
-describe('getGatingStatusFromJira suite', () => {
+describe('deleteDeployments suite', () => {
 	it('Should throw an error if no cloudId is passed', async () => {
-		const error = new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES);
-		await expect(getGatingStatusFromJira(null!, '1234', '1234', '1234')).rejects.toThrow(error);
+		(global as any).api = {
+			asApp: () => ({
+				__requestAtlassian: () => ({})
+			})
+		};
+
+		expect(async () => {
+			// @ts-ignore
+			await getGatingStatusFromJira(null, '1234', '1234', '1234');
+		}).rejects.toThrow(new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES));
 	});
 
 	it('Should throw an error if no deploymentId is passed', async () => {
-		const error = new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES);
-		await expect(getGatingStatusFromJira('1234', null!, '1234', '1234')).rejects.toThrow(error);
+		(global as any).api = {
+			asApp: () => ({
+				__requestAtlassian: () => ({})
+			})
+		};
+
+		expect(async () => {
+			// @ts-ignore
+			await getGatingStatusFromJira('1234', null, '1234', '1234');
+		}).rejects.toThrow(new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES));
 	});
 
 	it('Should throw an error if no pipelineId is passed', async () => {
-		const error = new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES);
-		await expect(getGatingStatusFromJira('1234', '1234', null!, '1234')).rejects.toThrow(error);
+		(global as any).api = {
+			asApp: () => ({
+				__requestAtlassian: () => ({})
+			})
+		};
+
+		expect(async () => {
+			// @ts-ignore
+			await getGatingStatusFromJira('1234', '1234', null, '1234');
+		}).rejects.toThrow(new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES));
 	});
 
 	it('Should throw an error if no environmentId is passed', async () => {
-		const error = new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES);
-		await expect(getGatingStatusFromJira('1234', '1234', '1234', null!)).rejects.toThrow(error);
+		(global as any).api = {
+			asApp: () => ({
+				__requestAtlassian: () => ({})
+			})
+		};
+
+		expect(async () => {
+			// @ts-ignore
+			await getGatingStatusFromJira('1234', '1234', '1234', null);
+		}).rejects.toThrow(new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES));
 	});
 
 	it('Should return status with empty body if no responseString is returned', async () => {
-		const mockResponse = { status: 200, text: jest.fn() };
-		api.asApp().requestConnectedData = jest.fn().mockImplementation(() => ({
-			then: (callback: any) => Promise.resolve(callback(mockResponse))
-		}));
+		(global as any).api = {
+			asApp: () => ({
+				__requestAtlassian: () => ({ status: 200, text: jest.fn() })
+			})
+		};
 
 		const response = await getGatingStatusFromJira('1234', '1234', '1234', '1234');
 		expect(response).toEqual({ status: 200, body: {} });
 	});
 
 	it('Should return status with response body when responseString is returned', async () => {
-		const resPayload = {
-			data: {
-				response: {
-					acceptedBuilds: [],
-					rejectedBuilds: [],
-					acceptedDeployments: [],
-					rejectedDeployments: []
+		const resPayload =
+			{
+				data: {
+					response: {
+						acceptedBuilds: [],
+						rejectedBuilds: [],
+						acceptedDeployments: [],
+						rejectedDeployments: []
+					}
 				}
-			}
+			};
+
+		(global as any).api = {
+			asApp: () => ({
+				__requestAtlassian: () => ({
+					status: 200,
+					text: jest.fn(() => {
+						return Promise.resolve(JSON.stringify({
+							...resPayload
+						}));
+					})
+				})
+			})
 		};
-
-		const mockText = jest.fn(() => {
-			return Promise.resolve(JSON.stringify({
-				...resPayload
-			}));
-		});
-
-		const mockResponse = { status: 200, text: mockText };
-		api.asApp().requestConnectedData = jest.fn().mockImplementation(() => ({
-			then: (callback: any) => Promise.resolve(callback(mockResponse))
-		}));
 
 		const response = await getGatingStatusFromJira('1234', '1234', '1234', '1234');
 		expect(response).toEqual({ status: 200, body: { data: resPayload.data } });
--- app/src/jira-client/get-gating-status-from-jira.ts
+++ app/src/jira-client/get-gating-status-from-jira.ts
@@ -1,4 +1,3 @@
-import api, { route } from '@forge/api';
 import { JiraResponse } from './types';
 import { InvalidPayloadError } from '../common/error';
 import { Errors } from '../common/error-messages';
@@ -18,12 +17,21 @@ async function getGatingStatusFromJira(
 		throw new InvalidPayloadError(Errors.MISSING_REQUIRED_PROPERTIES);
 	}
 
+	// I would have liked to use Forge's `route` template to create the URL
+	// (https://developer.atlassian.com/platform/forge/runtime-reference/product-fetch-api/#route),
+	// but it fails with the error described here:
 	// eslint-disable-next-line max-len
-	const getGatingStatusRoute = route`/deployments/0.1/cloud/${encodeURIComponent(cloudId)}/pipelines/${encodeURIComponent(pipelineId)}/environments/${encodeURIComponent(environmentId)}/deployments/${encodeURIComponent(deploymentId)}/gating-status`;
+	// https://community.developer.atlassian.com/t/problems-with-part-2-call-a-jira-api-in-forge-developer-tutorial/48818.
+	// Probably it's not compatible with the `__requestAtlassian()` function?
+	// Workaround: call `encodeURIComponent()` on each parameter by hand.
+	// eslint-disable-next-line max-len
+	const getGatingStatusRoute = `/jira/deployments/0.1/cloud/${encodeURIComponent(cloudId)}/pipelines/${encodeURIComponent(pipelineId)}/environments/${encodeURIComponent(environmentId)}/deployments/${encodeURIComponent(deploymentId)}/gating-status`;
 
-	const apiResponse = await api
+	// @ts-ignore // required so that Typescript doesn't complain about the missing "api" property
+	// eslint-disable-next-line no-underscore-dangle
+	const apiResponse = await global.api
 		.asApp()
-		.requestConnectedData(getGatingStatusRoute, {
+		.__requestAtlassian(getGatingStatusRoute, {
 			method: 'GET',
 			headers: {
 				'content-type': 'application/json'
