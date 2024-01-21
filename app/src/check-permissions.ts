a6d01d4b03e44b26333e62044fa4786482162a08
Rachelle Rathbone <rachellerathbone@gmail.com>
2024-01-22 09:38:40 +1100

    clean up

--- app/src/check-permissions.ts
+++ app/src/check-permissions.ts
@@ -1,17 +1,15 @@
 import api, { route } from '@forge/api';
-import { GLOBAL_PAGE } from '../jenkins-for-jira-ui/src/common/constants';
 
 // Forge does not export their Request type, so we have to resort to 'any' for now.
 export const adminPermissionCheck = async (req: any): Promise<void> => {
-	const { accountId, moduleKey } = req.context;
-	const isGlobalPage = moduleKey === GLOBAL_PAGE;
+	const { accountId } = req.context;
 
 	const permissionRequestBody = `{
-      "globalPermissions": [
-         "ADMINISTER"
-      ],
-      "accountId": "${accountId}"
-   }`;
+		"globalPermissions": [
+			"ADMINISTER"
+		],
+		"accountId": "${accountId}"
+	}`;
 
 	const permissions = await api.asUser().requestJira(route`/rest/api/3/permissions/check`, {
 		method: 'POST',
@@ -22,13 +20,13 @@ export const adminPermissionCheck = async (req: any): Promise<void> => {
 		body: permissionRequestBody
 	});
 
-	if (permissions.status === 403 && !isGlobalPage) {
+	if (permissions.status === 403) {
 		throw new Error('Only Jira administrators can access the Jenkins for Jira admin page.');
 	}
 
 	const permissionDetails = await permissions.json();
 
-	if (!permissionDetails?.globalPermissions?.includes('ADMINISTER') && !isGlobalPage) {
+	if (!permissionDetails?.globalPermissions?.includes('ADMINISTER')) {
 		throw new Error('Only Jira administrators can access the Jenkins for Jira admin page.');
 	}
 };
