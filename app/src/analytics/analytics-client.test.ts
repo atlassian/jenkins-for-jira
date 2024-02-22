cc2e8a2747abd0d8e273ae9f1de73387b1ff9960
Josh Kay <jkay@atlassian.com>
2024-02-22 23:59:27 +1300

    added manual analytics

--- app/src/analytics/analytics-client.test.ts
+++ app/src/analytics/analytics-client.test.ts
@@ -1,11 +1,13 @@
-import { isProductionEnv, createAnonymousId, getAccountDetails } from './analytics-client';
+import fetch from 'node-fetch';
+import {
+    sendAnalytics,
+    createAnonymousId,
+    createTrackEvent,
+    createMessageId,
+    isProductionEnv
+} from './analytics-client';
 
-jest.mock('./analytics-client', () => ({
-    ...jest.requireActual('./analytics-client'),
-    getAnalyticsClient: jest.fn().mockReturnValue({ cats: 'ssss' })
-}));
-
-jest.mock('@atlassiansox/analytics-node-client');
+jest.mock('node-fetch', () => jest.fn());
 
 describe('isProductionEnv', () => {
     test('should return true for production environment', () => {
@@ -23,18 +25,33 @@ describe('createAnonymousId', () => {
         expect(result).toBe('620ae460798e1f4cab44c44f3085620284f0960a276bbc3f0bd416449df14dbe');
     });
 });
-describe('getAccountDetails', () => {
-    test('should return anonymousId when no accountId', () => {
-        const result = getAccountDetails('', 'ipAddress');
-        expect(result).toEqual({
-            anonymousId: '095cf3ff5683bd7c1db48018af7d0bee79205bcafb47a09b97f9faf0bcee4800'
-        });
+
+describe('sendAnalytics', () => {
+    it('should send analytics', async () => {
+        const eventPayload = { eventName: 'event', action: 'action', actionSubject: 'subject' };
+        await sendAnalytics('cloudId', eventPayload, 'accountId', 'anonymousId');
+        expect(fetch).toHaveBeenCalled();
     });
-    test('should return accountId object', () => {
-        const result = getAccountDetails('testAccount');
-        expect(result).toEqual({
-            userIdType: 'atlassianAccount',
-            userId: 'testAccount',
-        });
+});
+
+describe('createTrackEvent', () => {
+    it('should create an event', () => {
+        const eventPayload = { eventName: 'event', action: 'action', actionSubject: 'subject' };
+        const event = createTrackEvent('cloudId', eventPayload, 'accountId', 'anonymousId');
+        expect(event).toHaveProperty('userId', 'accountId');
+        expect(event).toHaveProperty('anonymousId');
+        expect(event).toHaveProperty('event');
+        expect(event).toHaveProperty('properties');
+        expect(event).toHaveProperty('type', 'track');
+        expect(event).toHaveProperty('timestamp');
+        expect(event).toHaveProperty('messageId');
+    });
+});
+
+describe('createMessageId', () => {
+    it('should create a messageId', () => {
+        const messageId = createMessageId();
+        expect(messageId).toBeDefined();
+        expect(messageId).toMatch(/^j4j-be-\d+-\d+$/);
     });
 });
--- app/src/analytics/analytics-client.ts
+++ app/src/analytics/analytics-client.ts
@@ -1,5 +1,4 @@
 import crypto from 'crypto';
-// import { fetch } from '@forge/api';
 import fetch from 'node-fetch';
 
 interface AnalyticsAttributes {
@@ -13,12 +12,53 @@ interface EventPayload {
 	attributes?: AnalyticsAttributes
 }
 
-enum EnvType {
-	LOCAL = 'local',
-	DEV = 'dev',
-	STAGING = 'stg',
-	PROD = 'prod'
-}
+interface Event {
+	userId?: string;
+	anonymousId: string;
+	event: string;
+	properties: {
+		action: string;
+		actionSubject: string;
+		attributes: Record<string, string>;
+		env: string;
+		eventType: string;
+		origin: string;
+		product: string;
+		source: string;
+		tenantId: string;
+		tenantIdType: string;
+		userIdType?: string;
+	};
+	type: string;
+	timestamp: string;
+	messageId: string;
+  }
+
+interface Payload {
+	batch: Event[];
+	timestamp: string;
+	sentAt: string;
+  }
+
+interface Options {
+	method: string;
+	body: string;
+	headers: {
+		'Content-Type': string;
+	};
+  }
+
+  const environments = {
+	LOCAL: 'local',
+	DEV: 'dev',
+	STAGING: 'stg',
+	PROD: 'prod'
+  };
+
+  const TRACK_EVENT_TYPE = 'track';
+  const PRODUCT = 'jenkinsForJira';
+  const ORIGIN = 'server';
+  const TENANT_ID_TYPE = 'cloudId';
 
 // eslint-disable-next-line max-len
 export const sendAnalytics = async (cloudId: string, eventPayload: EventPayload, accountId?: string, anonymousId?: string): Promise<void> => {
@@ -28,169 +68,66 @@ export const sendAnalytics = async (cloudId: string, eventPayload: EventPayload,
 		.catch((e) => console.error({ e }, 'Failed to send analytics event'));
 };
 
+// TODO BETTER NAMING AND PULL FROM ENVVARS TO HIDE PATH
+const getAnalyticsEnvironmentUrl = () => {
+	// if is prod
+	if (isProductionEnv()) {
+        return 'https://as.atlassian.com/api';
+	}
+	return 'https://as.staging.atl-paas.net/api/v1/batch';
+};
+
 // eslint-disable-next-line max-len,@typescript-eslint/no-unused-vars
 const sendEvent = async (cloudId: string, eventPayload: EventPayload, accountId?: string, anonymousId?: string): Promise<void> => {
-	// const analyticsClient = await getAnalyticsClient();
-	// const {
-	// 	eventName, attributes, actionSubject, action
-	// } = eventPayload;
-
-	// if (!analyticsClient) {
-	// 	// if (!analyticsClient || !isProductionEnv()) {
-	// 	console.warn('Analytics sendEvent skipped:
-	// 	@atlassiansox/analytics-node-client module not found or environment not production.');
-	// 	return;
-	// }
-
-    console.warn('analyticsClient found');
-    console.warn('analyticsClient found');
-    console.warn('analyticsClient found');
-
-	// const accountDetails = getAccountDetails(accountId, anonymousId);
-
-	// todo call nv type to return prod or this from envvars
-	const url = 'https://as.staging.atl-paas.net/api/v1/batch';
-    //
-	// const payload = {
-	// 	batch: [
-	// 		{
-	// 			userId: '70121:3e65d00a-afa5-465f-b7b4-b92665c1dc92',
-	// 			anonymousId: undefined,
-	// 			event: 'User action Connected new Jenkins server',
-	// 			properties: [Object],
-	// 			timestamp: '2024-02-21T04:13:44.130Z',
-	// 			context: [Object],
-	// 			type: 'track',
-	// 			_metadata: [Object],
-	// 			messageId: 'node-b207547505dbc2307c66fca5d7c195e6-7445b312-c229-4e85-83a4-b43c4e1671a6'
-	// 		}
-	// 	],
-	// 	timestamp: '2024-02-21T04:13:44.132Z',
-	// 	sentAt: '2024-02-21T04:13:44.132Z'
-	// };
-
-    console.warn('HERE GOES FRESH STYLE');
-
+	const url = getAnalyticsEnvironmentUrl();
+	const trackEvent: Event = createTrackEvent(cloudId, eventPayload, accountId, anonymousId);
 	const timestamp = new Date().toISOString();
-
-	const d = {
-		batch: [
-			{
-				userId: '70121:3e65d00a-afa5-465f-b7b4-b92665c1dc92',
-				event: 'User Action Disconnected Jenkins Server',
-				properties: {
-					source: 'DisconnectedServer',
-					action: 'Disconnected Jenkins Server',
-					actionSubject: 'User Action',
-					attributes: {},
-					product: 'jenkinsForJira',
-					env: 'stg',
-					eventType: 'track',
-					userIdType: 'atlassianAccount',
-					tenantIdType: 'cloudId',
-					tenantId: '03ce73f8-41fc-492e-9983-83ab6d8ebd32',
-					origin: 'server'
-				},
-				timestamp,
-				type: 'track',
-				messageId: 'node-1f20e23f793c98e711461e82dc555521-2d8ce23f-cdd6-48be-8935-cd32c36cd6f7'
-			}
-		],
+	const payload: Payload = {
+		batch: [trackEvent],
 		timestamp,
 		sentAt: timestamp
 	};
 
-    // how to gen id
-    // message.messageId = `node-${md5(JSON.stringify(message))}-${uuid()}`
-	const options = {
+	const options: Options = {
 		method: 'POST',
-		body: JSON.stringify(d),
+		body: JSON.stringify(payload),
 		headers: {
 			'Content-Type': 'application/json'
-			// 'Authorization': 'Basic ' + new Buffer('BLANK' + ':' + '').toString('base64'),
-			// 'User-Agent': 'analytics-node/6.2.0'
 		}
 	};
 
-	console.warn('XXXXPRE FETCHRE GOES FRESH STYLE');
+	console.info('Sending Analytics');
+	await fetch(url, options);
+};
 
-	// eslint-disable-next-line @typescript-eslint/no-shadow
-	async function fetchData(url: string, options: any) {
-		try {
-			const res = await fetch(url, options);
-			const json = await res.json();
-			console.info('wow');
-			console.info('wow');
-			console.info(json);
-		} catch (err) {
-			console.error(`error:${err}`);
-		}
-	}
+// eslint-disable-next-line max-len
+export const createTrackEvent = (cloudId: string, eventPayload: EventPayload, accountId?: string, anonymousId?: string): Event => {
+	const timestamp = new Date().toISOString();
+	const {
+		eventName, attributes, actionSubject, action
+	} = eventPayload;
 
-	await fetchData(url, options);
-	//
-    // console.warn('PRE FETCHRE GOES FRESH STYLE');
-	// fetch(url, options)
-	// 	.then((res) => res.json())
-	// 	.then((json) => {
-	// 		console.info('wow');
-	// 		console.info(json);
-	// 	})
-	// 	.catch((err) => console.error(`error:${err}`));
-
-	// try {
-	//     await analyticsClient.sendTrackEvent({
-	//         ...accountDetails,
-	//         tenantIdType: 'cloudId',
-	//         tenantId: cloudId,
-	//         trackEvent: {
-	//             source: eventName,
-	//             action: action || 'unknown',
-	//             actionSubject: actionSubject || eventName,
-	//             attributes: {
-	//                 ...attributes
-	//             }
-	//         }
-	//     });
-	// } catch (e) {
-	//     console.error(e);
-	// }
-	//
-	// const body = {
-	//     jiraHost: 'https://jkay.jira-dev.com',
-	//     eventPayload: {
-	//         eventType: 'screen',
-	//         eventProps: {
-	//             name: 'banananas',
-	//             fooProp: 'bar'
-	//         },
-	//         eventAttributes: {
-	//             attr1: 'value1'
-	//         }
-	//     }
-	// };
-	//
-	//
-	// const resulthc = await fetch(
-	//     'https://jenkins-for-jira-analytics.dev.services.atlassian.com/openapi.json'
-	// );
-	//
-	// console.log('resulthc');
-	// console.log('resulthc');
-	// console.log(resulthc);
-	//
-	// const result = await fetch(
-	// 	'https://jenkins-for-jira-analytics.dev.services.atlassian.com/analytics', {
-	//         method: 'POST',
-	//         body: JSON.stringify(body),
-	//         headers: { 'Content-Type': 'application/json' }
-	//     }
-	// );
-	//
-	// const { status } = result;
-	// console.log('GOT ME A STATUS');
-	// console.log(status);
-	// console.log(result);
+	return {
+		userId: accountId,
+		anonymousId: createAnonymousId(anonymousId || 'default'),
+		event: `${actionSubject} ${action}`,
+		properties: {
+			source: eventName,
+			action,
+			actionSubject,
+			attributes: attributes || {},
+			product: PRODUCT,
+			env: isProductionEnv() ? environments.PROD : environments.STAGING,
+			eventType: TRACK_EVENT_TYPE,
+			userIdType: accountId ? 'atlassianAccount' : undefined, // TODO DO BETTER
+			tenantIdType: TENANT_ID_TYPE,
+			tenantId: cloudId,
+			origin: ORIGIN
+		},
+		type: TRACK_EVENT_TYPE,
+		timestamp,
+		messageId: createMessageId()
+	};
 };
 
 export const isProductionEnv = (): boolean => {
@@ -198,33 +135,13 @@ export const isProductionEnv = (): boolean => {
 	return ['production', 'prod'].includes(env);
 };
 
-export const getAccountDetails = (accountId?: string, anonymousId?: string) => {
-	if (accountId) {
-		return {
-			userIdType: 'atlassianAccount',
-			userId: accountId
-		};
-	}
-	const hashedAnonymousId = createAnonymousId(anonymousId || 'default');
-	return { anonymousId: hashedAnonymousId };
-};
-
 export const createAnonymousId = (input: string): string => {
 	const hash = crypto.createHash('sha256').update(input).digest('hex');
 	return hash;
 };
 
-export const getAnalyticsClient = async (): Promise<any> => {
-	try {
-		const { analyticsClient } = await import('@atlassiansox/analytics-node-client');
-
-		const analyticsNodeClient = analyticsClient({
-			env: EnvType.STAGING,
-			product: 'jenkinsForJira'
-		});
-
-		return analyticsNodeClient;
-	} catch (error) {
-		return null;
-	}
+export const createMessageId = () => {
+	const now = new Date().valueOf();
+	const random = Math.floor(Math.random() * 1000000);
+	return `j4j-be-${now}-${random}`;
 };
