8e6e4cd51561e10076d4d93cb0c37205d22519de
Josh Kay <jkay@atlassian.com>
2024-02-28 11:45:23 +1300

    removed the last page of the jenkoins workflow

--- app/jenkins-for-jira-ui/src/components/JenkinsSetup/JenkinsSetup.tsx
+++ app/jenkins-for-jira-ui/src/components/JenkinsSetup/JenkinsSetup.tsx
@@ -320,26 +320,15 @@ const JenkinsSetup = (): JSX.Element => {
 		);
 	};
 
-	const handleNavigateToConnectionCompleteScreen = (e: React.MouseEvent) => {
+	const handleNavigateToConnectionServerManagementScreen = (e: React.MouseEvent) => {
 		e.preventDefault();
+		console.log('path', path);
 
-		let pathParam = '';
-
-		if (showMyJenkinsAdmin) {
-			pathParam = 'requires-jenkins-admin';
-		} else {
-			pathParam = 'is-admin';
+		if (path === 'global') {
+			router.navigate(globalPageUrl);
 		}
 
-		if (connectionSettings) {
-			if (path === 'admin') {
-				history.push('/');
-			} else {
-				router.navigate(globalPageUrl);
-			}
-		} else {
-			history.push(`/connection-complete/${uuid}/${pathParam}/${path}`);
-		}
+		history.push('/');
 	};
 
 	const isFetchingData = !serverName || !webhookUrl || !secret;
@@ -403,7 +392,7 @@ const JenkinsSetup = (): JSX.Element => {
 								<Button
 									type="button"
 									appearance="primary"
-									onClick={(e) => handleNavigateToConnectionCompleteScreen(e)}
+									onClick={(e) => handleNavigateToConnectionServerManagementScreen(e)}
 									testId="jenkins-set-up-next-btn"
 								>
 									{connectionSettings ? 'Done' : 'Next'}
